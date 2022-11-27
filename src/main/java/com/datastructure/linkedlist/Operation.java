fd9173d3499336716eb8415dde6dacda39c3c145
Kunal <kunalsonawane730@gmail.com>
2022-11-27 10:03:24 +0530

    program to create linkedlist by inserting element in between

--- src/main/java/com/datastructure/linkedlist/Operation.java
+++ src/main/java/com/datastructure/linkedlist/Operation.java
@@ -19,4 +19,17 @@ public class Operation {
         linkedList.print();
 
 }
+  //created method for insert in between
+    public static void insertInBetween(){
+        LinkedList linkedList=new LinkedList();
+        Node secondNode =linkedList.push(70);
+        Node firstNode =linkedList.push(56);
+        Node newNode =new Node(30);
+        System.out.println("Before");
+        linkedList.print();
+        linkedList.insertInBetween(firstNode,newNode);
+        System.out.println(                             );
+        System.out.println("After");
+        linkedList.print();
+}
 }
